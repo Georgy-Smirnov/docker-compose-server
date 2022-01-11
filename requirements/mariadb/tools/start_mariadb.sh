022a143be76d63c4ad377ed0aea711492b510cc6
Kandace Jaco <kjaco@ge-n6.kzn.21-school.ru>
2022-01-11 16:50:48 +0300

    second

--- requirements/mariadb/tools/start_mariadb.sh
+++ requirements/mariadb/tools/start_mariadb.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+if ! [ -d /var/lib/mysql/wordpress ] ; then 
 service mysql start
 envsubst < /create_database.sql | mysql
 mysqladmin -u root password "$DB_ROOT_PASSWORD"
 service mysql stop
-
-mysqld
+fi
+chown -R mysql:mysql /var/lib/mysql
+/usr/bin/mysqld_safe
